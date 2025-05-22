769475f5d723a6f42d3891cc6ce8610991e7d0aa
Tobiloba Sunday <sundaytobiloba2889@gmail.com>
2025-05-22 21:30:47 +0100

    testing git commit

---
+++ Assignment6.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+// This is a test for a github commit
+contract tech4devone{
+ 
+    // Create a function that consist of if and else statement, and give an example with it.
+    function conditional(uint _y) public pure returns(uint){
+        if(_y <=5){
+        return 6;
+ 
+        } else{
+        return 1;
+        }
+    }
+ 
+}
